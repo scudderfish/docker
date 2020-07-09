deb52e0302a1890b7d87ac7c8a5089368948b4f6
David Smith <dave@smithfamily.org.uk>
2020-07-09 10:36:05 +0100

    Change to non-commercial OpenALPR and pull the webservice in for one package

--- webservice/test_single_image.sh
+++ webservice/test_single_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s -X POST -F name=file -F "image=@sample.jpg" 'http://localhost:8888/v1/identify/plate?country=us'
+curl -X POST -F name=file -F "image=@sample.jpg" 'http://localhost:8000/v1/identify/plate?country=us'

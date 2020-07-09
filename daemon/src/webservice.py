fae259bcee6d70573f3907de7a48567a87cc56cb
David Smith <dave@smithfamily.org.uk>
2020-07-09 15:11:39 +0100

    A working webserver

--- daemon/src/webservice.py
+++ daemon/src/webservice.py
@@ -1,167 +1,76 @@
-
-
+import cgi
 import json
-import tornado.ioloop
-import tornado.web
-from tornado import gen
-import time
-from tornado.concurrent import run_on_executor
-from concurrent.futures import ThreadPoolExecutor
+import re
 import threading
-import multiprocessing
-
 from argparse import ArgumentParser
+from cgi import parse_header
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
 
 from openalpr import Alpr
 
-
 HTTP_CODE_BAD_REQUEST = 400
 
-if __name__ == "__main__":
-
-    parser = ArgumentParser(description='TrueCar Computer Vision Webservice')
-
-    parser.add_argument("-p", "--port", dest="port", action="store", metavar='port', type=int, required=False, default=8888,
-                      help="Port to listen on" )
-
-    parser.add_argument("-t", "--threads", dest="threads", action="store", metavar='threads', type=int, required=False, default=2,
-                      help="Number of processing threads to handle computer vision jobs.  Should not exceed number of CPU cores" )
-
-    parser.add_argument('-d', '--debug', action='store_true', default=False,
-                        help="Show debug output")
+alpr = Alpr(country="gb", config_file="/etc/openalpr/openalpr.conf", runtime_dir="/usr/share/openalpr/runtime_data")
 
 
-    options = parser.parse_args()
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_POST(self):
+        jsontxt = "{}"
+        if None != re.search('/api/v1/identify', self.path):
+            ctype, pdict = parse_header(self.headers['content-type'])
+            pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+            pdict['CONTENT-LENGTH'] = self.headers.get_content_type()
+            if ctype == 'multipart/form-data':
+                postvars = cgi.parse_multipart(self.rfile, pdict)
 
-    debug = options.debug
+                image_ = postvars['image'][0]
 
-    if options.threads > multiprocessing.cpu_count():
-        print "Warning, attempting to use %d threads when your system only has %d cores" % (options.threads, multiprocessing.cpu_count())
-
-    executor = ThreadPoolExecutor(options.threads)
-
-
-class AlprHandler(tornado.web.RequestHandler):
-
-    # Pull the executor into the class
-    executor = executor
-
-    alpr_processes = {}
-
-
-    @gen.coroutine
-    def post(self):
-
-
-        start = time.clock()
-        response = {
-            'version': 1,
-        }
-
-        if 'image' not in self.request.files:
-            response['error'] = 'image_missing'
-            self.finish(json.dumps(response))
-            return
-
-        topn = int(self.get_argument("topn", default=20, strip=True))
-        state = self.get_argument("state", default="", strip=True)
-
-        if len(state) > 0 and len(state) != 2:
-            self.set_status(HTTP_CODE_BAD_REQUEST)
-            response['error'] = 'invalid_state'
-            self.finish(json.dumps(response))
-            return
-
-        if topn < 0 or topn > 10000:
-            self.set_status(HTTP_CODE_BAD_REQUEST)
-            response['error'] = 'invalid_topn'
-            self.finish(json.dumps(response))
-            return
-
-        fileinfo = self.request.files['image'][0]
-        jpeg_bytes = fileinfo['body']
+                data = alpr.recognize_array(image_)
+                jsontxt = json.dumps(data)
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(jsontxt.encode(encoding='utf_8'))
+        else:
+            self.send_response(403)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+        return
 
 
-        if len(jpeg_bytes) <= 0:
-            self.set_status(HTTP_CODE_BAD_REQUEST)
-            response['error'] = 'invalid_image_data'
-            self.finish(json.dumps(response))
-            return
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    allow_reuse_address = True
+
+    def shutdown(self):
+        self.socket.close()
+        HTTPServer.shutdown(self)
 
 
-        alpr_results = yield self.alpr_processor(jpeg_bytes, topn, state)
+class SimpleHttpServer:
+    def __init__(self, ip, port):
+        self.server = ThreadedHTTPServer((ip, port), HTTPRequestHandler)
+        self.server_thread = threading.Thread(target=self.server.serve_forever)
 
+    def start(self):
+        self.server_thread.daemon = True
+        self.server_thread.start()
 
-        end = time.clock()
-        if debug:
-            print "Total POST time: %.2f ms" % ((end - start) * 1000)
+    def waitForThread(self):
+        self.server_thread.join()
 
-        self.finish(json.dumps(alpr_results))
+    def stop(self):
+        self.server.shutdown()
+        self.waitForThread()
 
-    @run_on_executor(executor='executor')
-    def alpr_processor(self, image, topn, state):
-
-        thread_id = threading.currentThread().ident
-
-        if thread_id not in self.alpr_processes:
-            if debug:
-                print "Kicking off new ALPR process"
-            self.alpr_processes[thread_id] = Alpr("us", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
-            self.alpr_processes[thread_id].set_detect_region(True)
-
-        self.alpr_processes[thread_id].set_top_n(topn)
-        if state is not None and state != "":
-            self.alpr_processes[thread_id].set_default_region(state)
-
-        if debug:
-            print "Starting alpr job"
-            print "back-queue size: %d" % executor._work_queue.qsize(),
-
-            print "args: topn %d, state: %s" % (topn, state)
-            print "Thread ID: " + str (threading.currentThread().ident)
-        try:
-            start = time.clock()
-            results = self.alpr_processes[thread_id].recognize_array(image)
-            end = time.clock()
-
-            if debug:
-                print "ALPR Processing time: %.2f ms" % ((end - start) * 1000)
-            return results
-        except:
-            return {'error': 'alpr_processing_error'}
-
-
-class InfoHandler(tornado.web.RequestHandler):
-
-
-    def get(self):
-
-        response = {
-            'queue_size': executor._work_queue.qsize(),
-            'threads': len(executor._threads)
-        }
-
-        self.finish(json.dumps(response))
-
-
-class HealthcheckHandler(tornado.web.RequestHandler):
-
-
-    def get(self):
-        self.set_status(200)
-        self.finish("")
-
-application = tornado.web.Application([
-    (r"/v1/identify/plate", AlprHandler),
-    (r"/v1/info", InfoHandler),
-    (r"/v1/healthcheck", HealthcheckHandler),
-
-])
 
 if __name__ == "__main__":
+    parser = ArgumentParser(description='HTTP Server')
+    parser.add_argument('port', type=int, help='Listening port for HTTP Server', default=8080)
+    parser.add_argument('ip', help='HTTP Server IP', default="127.0.0.1")
+    args = parser.parse_args()
 
-    print "OpenALPR Web server started on port %d" % (options.port)
-    print "Using %d parallel ALPR threads" % (options.threads)
-    application.listen(options.port)
-    tornado.ioloop.IOLoop.current().start()
-
+    server = SimpleHttpServer(args.ip, args.port)
+    print('HTTP Server Running...........')
+    server.start()
+    server.waitForThread()
